ad2ac2ceaa1a8bd837d57f86d19027e0b3bc0003
Anirudha Kulkarni <kulkarnianirudha8@gmail.com>
2023-08-28 11:05:42 +0530

    Create CubicProbing.cpp

---
+++ CubicProbing.cpp
@@ -0,0 +1,38 @@
+#include "CubicProbing.h"
+
+void CubicProbing::createAccount(std::string id, int count) {
+    // IMPLEMENT YOUR CODE HERE
+}
+
+std::vector<int> CubicProbing::getTopK(int k) {
+    // IMPLEMENT YOUR CODE HERE
+    return std::vector<int>(); // Placeholder return value
+}
+
+int CubicProbing::getBalance(std::string id) {
+    // IMPLEMENT YOUR CODE HERE
+    return 0; // Placeholder return value
+}
+
+void CubicProbing::addTransaction(std::string id, int count) {
+    // IMPLEMENT YOUR CODE HERE
+}
+
+bool CubicProbing::doesExist(std::string id) {
+    // IMPLEMENT YOUR CODE HERE
+    return false; // Placeholder return value
+}
+
+int CubicProbing::databaseSize() {
+    // IMPLEMENT YOUR CODE HERE
+    return 0; // Placeholder return value
+}
+
+int CubicProbing::hash(std::string id) {
+    // IMPLEMENT YOUR CODE HERE
+    return 0; // Placeholder return value
+}
+
+void CubicProbing::cubicProbing(std::string id, int count) {
+    // IMPLEMENT YOUR CODE HERE
+}
